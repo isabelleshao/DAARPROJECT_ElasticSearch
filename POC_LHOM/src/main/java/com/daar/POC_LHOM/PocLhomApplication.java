c68bd23a2a52aa5573e0eb6bef7ed77efd31df53
isabelleshao <47239377+isabelleshao@users.noreply.github.com>
2021-10-25 10:01:40 +0200

    Initial

---
+++ POC_LHOM/src/main/java/com/daar/POC_LHOM/PocLhomApplication.java
@@ -0,0 +1,13 @@
+package com.daar.POC_LHOM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocLhomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocLhomApplication.class, args);
+	}
+
+}
---
+++ POC_LHOM/src/test/java/com/daar/POC_LHOM/PocLhomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daar.POC_LHOM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocLhomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
